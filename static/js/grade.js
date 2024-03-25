a4b623095a54a47cb98debf758ed2bd41935f2f4
PolloHacker <isaac.reyalves@gmail.com>
2024-03-25 19:45:11 -0300

    started curriculum grid view added course data from CS

---
+++ static/js/grade.js
@@ -0,0 +1,40 @@
+(() => {
+    const dataContainer = document.querySelector('.curriculum-container');
+
+    fetch('static/CompUF.csv')
+        .then(response => response.text())
+        .then(data => {
+
+            const rows = data.split('\n');
+            const headers = rows[0].split(',');
+            const content = rows.slice(1).map(row => {
+                const values = row.split(',');
+                const obj = {};
+                headers.forEach((header, index) => {
+                    obj[header] = values[index];
+                });
+                return obj;
+            });
+            console.log(content)
+
+            content.forEach((el) => {
+                console.log(el)
+                const card = document.createElement('div');
+                card.classList.add('secondary-container', 'on-secondary-container-text');
+
+                const title = document.createElement('p');
+                title.classList.add('title', 'headline-small');
+                title.textContent = el.grade;
+                card.appendChild(title);
+
+                const text = document.createElement('p');
+                text.classList.add('text', 'body-medium');
+                text.textContent = el.optativas;
+                card.appendChild(text);
+
+                dataContainer.appendChild(card);
+            })
+        })
+        .catch(error => console.error(error));
+
+})()
