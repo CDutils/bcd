bf499d4408825622e127bedf5c8fffcecea53a63
PolloHacker <isaac.reyalves@gmail.com>
2024-04-02 21:58:12 -0300

    added CD real data and parsed it

--- static/js/grade.js
+++ static/js/grade.js
@@ -1,14 +1,14 @@
 (() => {
     const dataContainer = document.querySelector('.grid-container');
 
-    fetch('static/CompUF.csv')
+    fetch('static/ementas.csv')
         .then(response => response.text())
         .then(data => {
 
             const rows = data.split('\n');
-            const headers = rows[0].split(',');
+            const headers = rows[0].split(';');
             const content = rows.slice(1).map(row => {
-                const values = row.split(',');
+                const values = row.split(';');
                 const obj = {};
                 headers.forEach((header, index) => {
                     obj[header] = values[index];
@@ -18,18 +18,18 @@
             console.log(content)
 
             content.forEach((el) => {
-                console.log(el)
+
                 const card = document.createElement('div');
                 card.classList.add('secondary-container', 'on-secondary-container-text');
 
                 const title = document.createElement('p');
                 title.classList.add('title', 'headline-small');
-                title.textContent = el.grade;
+                title.textContent = el["Nome"];
                 card.appendChild(title);
 
                 const text = document.createElement('p');
                 text.classList.add('text', 'body-medium');
-                text.textContent = el.optativas;
+                text.textContent = el["Código"];
                 card.appendChild(text);
 
                 dataContainer.appendChild(card);
