2c599267bef5874683e0c79d5126f4acc97c1270
PolloHacker <isaac.reyalves@gmail.com>
2024-04-20 19:44:24 -0300

    fixed minor layout shifts

--- static/js/grid/widthwarn.js
+++ static/js/grid/widthwarn.js
@@ -1,27 +1,22 @@
 (() => {
     const showWarningDialog = () => {
-        setTimeout(() => {
-            warningDialog.show();
-        }, 3000)
+        let screenWidth = window.innerWidth;
+        if (screenWidth <= 700 && gridTab.classList.contains('active-link')) {
+            setTimeout(() => {
+                warningDialog.show();
+            }, 3000)
+        }
     }
 
     const closeDialogButton = document.querySelector('.close-small-screen-dialog-button');
     const warningDialog = document.querySelector('.small-screen-dialog');
     const gridTab = document.querySelector('.nav-links li a[href="#curriculum-tab"]');
 
-    const observer = new MutationObserver(() => {
-        let screenWidth = window.innerWidth;
-        if (screenWidth < 500 && gridTab.classList.contains('active-link'))
-            showWarningDialog()
-    });
+    const observer = new MutationObserver(showWarningDialog);
 
     observer.observe(gridTab, { attributes: true, attributeFilter: ['class'] });
 
-    window.addEventListener('resize', () => {
-        let screenWidth = window.innerWidth;
-        if (screenWidth < 500 && gridTab.classList.contains('active-link'))
-            showWarningDialog()
-    });
+    window.addEventListener('resize', showWarningDialog);
 
     closeDialogButton.addEventListener('click', async () => {
         await warningDialog.close();
